717192febf2498d7e3ab61f61333c5f24bb5731c
Mariela Rojas Parra <mariela.rojas@correo.buap.mx>
2019-10-27 02:46:27 -0600

    se agregan vistas hasta capitulo 5

---
+++ resources/views/Actividad3_.blade.php
@@ -0,0 +1,123 @@
+@extends('plantillas.menu')
+@section('titulo','Título')
+
+@section('content')
+
+  <!-- Content Header (Page header) -->
+
+  <section class="content-header" align="center">
+        <!-- <h1>Nombre de la página<small>Algún texto pequeño</small></h1> -->
+        <h1>Consecuencias negativas</h1>
+        <ol class="breadcrumb"><!-- <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li><li><a href="#">Examples</a></li><li class="active">Blank page</li> --> </ol>
+  </section>
+
+  <!-- Main content -->
+
+  <section class="content">
+
+    <!-- Default box -->
+    <div class="box">
+      <div class="box-header with-border">
+        <!-- <h3 class="box-title">Para aprender</h3> -->
+
+        <!-- <div class="box-tools pull-right">
+
+        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
+        <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button></div> -->
+        </div>
+        <div class="box-body">
+          <div align='center'>
+            <!-- <img src="{{asset('paginas/p7.PNG')}}" class="img-fluid jetzoom"  data-jetzoom ="zoomImage:'{{asset('paginas/p7L.PNG')}}'" > -->
+            <div class="login-box ">
+              <img src="{{asset('paginas/7_.JPG')}}" class="img-fluid" style="width:100%;">
+              <br>
+              <br>
+              <p class="text-left"> Menciona algo que hayas aprendido de <code class="text-purple "><strong>Leo, pienso y escribo 5°</strong></code>y explicalo con tus propias palabras:</p>
+              <input type="text text-center" class="form-control" align="center" id="Entrada_Que_aprendi_del_Cuento" placeholder="Escribe aqui tu respuesta" value="{{((isset($datos->respuesta1))?$datos->respuesta1:'')}}">
+              <br>
+              <p class="text-left"> ¿Cómo te sientes con el valor que aprendiste hoy? Explícanos:</p>
+              <input type="text text-center" class="form-control" align="center" id="Entrada_Que_aprendi_del_Cuento" placeholder="Escribe aqui tu respuesta" value="{{((isset($datos->respuesta1))?$datos->respuesta1:'')}}">
+              <br>
+              <img src="{{asset('paginas/7_1.JPG')}}" class="img-fluid" style="width:100%;">
+              <br>
+              <br>
+              <p class="text-left"> <textarea name="mensaje" placeholder="Anota dos conclusiones importantes a las que llegó el grupo:" rows="5" cols="53"></textarea></p>
+              <button type="button" class="btn btn-block btn-default" id="Boton_Enviar_Respuestas()" onclick="almacenarInformacion()">Enviar respuestas</button>
+
+            </div>
+          </div>
+        </div>
+
+        <!-- /.box-body -->
+
+        <div class="box-footer" align="center">
+        <div class="btn-group">
+          <button type="button" class="btn btn-default" onclick="anterior(6)"><i class="fa  fa-arrow-left"></i></button>
+          <button type="button" class="btn btn-default" onclick="siguiente(8)"><i class="fa fa-arrow-right"></i></button>
+        </div>
+        </div>
+
+        <!-- /.box-footer-->
+
+    </div>
+
+    <!-- /.box -->
+
+  </section>
+  <!-- /.content -->
+@endsection
+
+@section('script')
+  <script type="text/javascript">
+    function siguiente(pagina){
+      location.href = '/pagina/'+pagina;
+     //alert("adelante");
+    }
+    function anterior(pagina){
+        location.href = '/pagina/'+pagina;
+        //alert("atras");
+      }
+
+    function almacenarInformacion(){
+      var respuesta1 = $('#Entrada_Que_aprendi_del_Cuento').val();
+      var respuesta2 = $('#Entrada_Valores_hoy').val();
+      var respuesta3 = $('#Entrada_Como_me_siento').val();
+      //$("#div_cuadro").hide();
+      var success;
+      var url = "/almacenar/pagina-8";
+      var dataForm = new FormData();
+      dataForm.append('respuesta1',respuesta1);
+      dataForm.append('respuesta2',respuesta2);
+      dataForm.append('respuesta3',respuesta3);
+      //lamando al metodo ajax
+
+      $.ajax({
+        url :url,
+        data : dataForm,
+        contentType:false,
+        processData:false,
+        headers:{
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          },
+        type: 'POST',
+        dataType : 'json',
+        beforeSend: function (){
+          $("#modalCarga").modal();
+        },
+        success : function(json){
+
+
+
+        },
+        error : function(xhr, status) {
+          $("#textoModalMensaje").text('Existió un problema con la operación');
+          $("#modalMensaje").modal();
+        },
+        complete : function(xhr, status){
+           $("#modalCarga").modal('hide');
+        }
+      });//*/
+    }
+
+  </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -32,13 +32,47 @@ Route::get('/pagina/{numero}', function () {
 });//*/
 
 Route::get('/pagina/1', function (){return view('pagina');});
-Route::get('/pagina/2', function (){return view('pagina2');});
-Route::get('/pagina/3', function (){return view('pagina3');});
+Route::get('/pagina/21', function (){return view('pagina2');});
+Route::get('/pagina/31', function (){return view('pagina3');});
 Route::get('/pagina/4', function (){return view('pagina4');});
 Route::get('/pagina/5', function (){return view('pagina5');});
 Route::get('/pagina/6', function (){return view('pagina6');});
 Route::get('/pagina/7', function (){return view('pagina7');});
 Route::get('/nahuatl', function (){return view('audio_nahuatl');});
+
+//Routes 5°
+Route::get('/pagina/1', function (){return view('pagina1_');});
+Route::get('/pagina/2', function (){return view('pagina2_');});
+Route::get('/pagina/3', function (){return view('pagina3_');});
+Route::get('/pagina/4', function (){return view('pagina4_');});
+Route::get('/pagina/5', function (){return view('Actividad1_');});
+Route::get('/pagina/6', function (){return view('Actividad2_');});
+Route::get('/pagina/7', function (){return view('Actividad3_');});
+Route::get('/pagina/8', function (){return view('Actividad4_');});
+Route::get('/pagina/9', function (){return view('pagina9_');});
+Route::get('/pagina/10', function (){return view('pagina10_');});
+Route::get('/pagina/11', function (){return view('pagina11_');});
+Route::get('/pagina/12', function (){return view('Actividad5_');});
+Route::get('/pagina/13', function (){return view('Actividad6_');});
+Route::get('/pagina/14', function (){return view('Actividad7_');});
+Route::get('/pagina/15', function (){return view('Actividad8_');});
+Route::get('/pagina/16', function (){return view('pagina16_');});
+Route::get('/pagina/17', function (){return view('pagina17_');});
+Route::get('/pagina/18', function (){return view('pagina18_');});
+Route::get('/pagina/19', function (){return view('Actividad9_');});
+Route::get('/pagina/20', function (){return view('Actividad10_');});
+Route::get('/pagina/21', function (){return view('Actividad11_');});
+Route::get('/pagina/22', function (){return view('Actividad12_');});
+Route::get('/pagina/23', function (){return view('pagina23_');});
+Route::get('/pagina/24', function (){return view('pagina24_');});
+Route::get('/pagina/25', function (){return view('pagina25_');});
+
+Route::get('/pagina/30', function (){return view('pagina30_');});
+Route::get('/pagina/31', function (){return view('pagina31_');});
+Route::get('/pagina/32', function (){return view('pagina32_');});
+
+
+//Fin routes 5|
 
 //Route::get('/pagina/Tema_1_Actividad', function (){return view('pagina8_Actividad');});
 Route::get('/pagina/Tema_1_Actividad', 'PaginasController@Pagina8');
