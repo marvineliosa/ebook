1d04212b40ddc817da2c5fec7741dbfa43a48ded
Marvin Eliosa Abaroa <marvineliosa@hotmail.com>
2020-01-18 16:19:36 -0600

    Commit con capitulos 16 al 21

---
+++ resources/views/Capitulo_21/Pagina_169.blade.php
@@ -0,0 +1,102 @@
+@extends('plantillas.menu')
+@section('titulo','Título')
+
+@section('content')
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>
+    "El alcoholismo en la adolescencia: un peligro"
+    <!-- <small>Algún texto pequeño</small> -->
+  </h1>
+  <ol class="breadcrumb">
+    <!-- <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="#">Examples</a></li>
+    <li class="active">Blank page</li> -->
+  </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+  <!-- Default box -->
+  <div class="box">
+      <div class="box-header with-border" align="center">
+      <!--<button type="submit" class="btn btn-success" onclick="sugerencia()">Sugerencias para papás</button>-->
+      <br>
+      <br>
+    <!--  <h3 class="box-title"><audio id="audio" controls>
+          <source type="audio/wav" src="{{asset('audio/Parte 0.wav')}}">
+        </audio></h3>-->
+
+      <!-- <div class="box-tools pull-right">
+        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                title="Collapse">
+          <i class="fa fa-minus"></i></button>
+        <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+          <i class="fa fa-times"></i></button>
+      </div> -->
+    </div>
+    <div class="box-body">
+      <div align='center'>
+        <img src="{{asset('paginas/Capitulo_21/Pagina_181.jpg')}}" class="img-fluid jetzoom"  data-jetzoom ="zoomImage:'{{asset('paginas/Capitulo_21/Pagina_181.jpg')}}'" style="max-width: 50%" > <!--LINEA TRABJA CON JETZOOM-->
+      </div>
+    </div>
+    <!-- /.box-body -->
+    <div class="box-footer" align="center">
+      <div class="btn-group">
+        <button type="button" class="btn btn-default" onclick="anterior(168)"><i class="fa  fa-arrow-left"></i></button>
+        <button type="button" class="btn btn-default" onclick="siguiente(170)"><i class="fa fa-arrow-right"></i></button>
+      </div>
+    </div>
+    <!-- /.box-footer-->
+  </div>
+  <!-- /.box -->
+
+</section>
+<!-- /.content -->
+
+<!-- Modal -->
+<div class="modal fade" id="ModalPadres" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" align="center">
+        <h1 class="modal-title" id="exampleModalLabel">Sugerencia para padres</h1>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+        <div align='center'>
+          <img src="{{asset('padres/Padres_0.PNG')}}" class="img-fluid jetzoom"  data-jetzoom ="zoomImage:'{{asset('padres/Padres_0.PNG')}}'"  style="max-width: 100%">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+@section('script')
+  <script type="text/javascript">
+    var ant_capitulo = 21;
+    var sig_capitulo = 21;
+    var libro = 1;
+    function siguiente(pagina)
+       {
+        location.href = '/'+ libro + '/' + sig_capitulo+ '/' +pagina;
+         //alert("adelante");
+       }
+     function anterior(pagina)
+        {
+          location.href = '/'+ libro + '/' + ant_capitulo+ '/' +pagina;
+          //alert("atras");
+        }  </script>
+  <!-- script zoom con jetzoom-->
+  <script type="text/javascript">
+   JetZoom.quickStart();
+  </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -145,88 +145,147 @@ Route::post('/almacenar/pagina-comentarios', 'PaginasController@AlmacenarPaginaC
 
 // Capitulos 8 al 15 *******************************************************************************************************
 
-    //Capitulo 8
-            Route::get('/5/8/63', function (){return view('Capitulo_8/pagina63');});
-            Route::get('/5/8/64', function (){return view('Capitulo_8/pagina64');});
-            Route::get('/5/8/65', function (){return view('Capitulo_8/pagina65');});
+  //Capitulo 8
+    Route::get('/5/8/63', function (){return view('Capitulo_8/pagina63');});
+    Route::get('/5/8/64', function (){return view('Capitulo_8/pagina64');});
+    Route::get('/5/8/65', function (){return view('Capitulo_8/pagina65');});
 
-            Route::get('/5/8/66', 'PaginasController@Pagina66');
+    Route::get('/5/8/66', 'PaginasController@Pagina66');
 
-            Route::get('/5/8/67', function (){return view('Capitulo_8/pagina67');});
-            Route::get('/5/8/68', function (){return view('Capitulo_8/pagina68');});
-            Route::get('/5/8/69', function (){return view('Capitulo_8/pagina69');});
+    Route::get('/5/8/67', function (){return view('Capitulo_8/pagina67');});
+    Route::get('/5/8/68', function (){return view('Capitulo_8/pagina68');});
+    Route::get('/5/8/69', function (){return view('Capitulo_8/pagina69');});
 
-    //Capitulo 9
-            Route::get('/5/9/70', function (){return view('Capitulo_9/pagina70');});
-            Route::get('/5/9/71', function (){return view('Capitulo_9/pagina71');});
-            Route::get('/5/9/72', function (){return view('Capitulo_9/pagina72');});
-            Route::get('/5/9/73', function (){return view('Capitulo_9/pagina73');});
-            Route::get('/5/9/74', function (){return view('Capitulo_9/pagina74');});
-            Route::get('/5/9/75', function (){return view('Capitulo_9/pagina75');});
-            Route::get('/5/9/76', function (){return view('Capitulo_9/pagina76');});
-            Route::get('/5/9/77', function (){return view('Capitulo_9/pagina77');});
-            Route::get('/5/9/78', function (){return view('Capitulo_9/pagina78');});
+  //Capitulo 9
+    Route::get('/5/9/70', function (){return view('Capitulo_9/pagina70');});
+    Route::get('/5/9/71', function (){return view('Capitulo_9/pagina71');});
+    Route::get('/5/9/72', function (){return view('Capitulo_9/pagina72');});
+    Route::get('/5/9/73', function (){return view('Capitulo_9/pagina73');});
+    Route::get('/5/9/74', function (){return view('Capitulo_9/pagina74');});
+    Route::get('/5/9/75', function (){return view('Capitulo_9/pagina75');});
+    Route::get('/5/9/76', function (){return view('Capitulo_9/pagina76');});
+    Route::get('/5/9/77', function (){return view('Capitulo_9/pagina77');});
+    Route::get('/5/9/78', function (){return view('Capitulo_9/pagina78');});
 
-    //Capitulo 10
-            Route::get('/5/10/79', function (){return view('Capitulo_10/pagina79');});
-            Route::get('/5/10/80', function (){return view('Capitulo_10/pagina80');});
-            Route::get('/5/10/81', function (){return view('Capitulo_10/pagina81');});
-            Route::get('/5/10/82', function (){return view('Capitulo_10/pagina82');});
-            Route::get('/5/10/83', function (){return view('Capitulo_10/pagina83');});
-            Route::get('/5/10/84', function (){return view('Capitulo_10/pagina84');});
-            Route::get('/5/10/85', function (){return view('Capitulo_10/pagina85');});
+  //Capitulo 10
+    Route::get('/5/10/79', function (){return view('Capitulo_10/pagina79');});
+    Route::get('/5/10/80', function (){return view('Capitulo_10/pagina80');});
+    Route::get('/5/10/81', function (){return view('Capitulo_10/pagina81');});
+    Route::get('/5/10/82', function (){return view('Capitulo_10/pagina82');});
+    Route::get('/5/10/83', function (){return view('Capitulo_10/pagina83');});
+    Route::get('/5/10/84', function (){return view('Capitulo_10/pagina84');});
+    Route::get('/5/10/85', function (){return view('Capitulo_10/pagina85');});
 
-    //Capitulo 11
+  //Capitulo 11
 
-            Route::get('/5/11/86', function (){return view('Capitulo_11/pagina86');});
-            Route::get('/5/11/87', function (){return view('Capitulo_11/pagina87');});
-            Route::get('/5/11/88', function (){return view('Capitulo_11/pagina88');});
-            Route::get('/5/11/89', function (){return view('Capitulo_11/pagina89');});
-            Route::get('/5/11/90', function (){return view('Capitulo_11/pagina90');});
-            Route::get('/5/11/91', function (){return view('Capitulo_11/pagina91');});
-            Route::get('/5/11/92', function (){return view('Capitulo_11/pagina92');});
+    Route::get('/5/11/86', function (){return view('Capitulo_11/pagina86');});
+    Route::get('/5/11/87', function (){return view('Capitulo_11/pagina87');});
+    Route::get('/5/11/88', function (){return view('Capitulo_11/pagina88');});
+    Route::get('/5/11/89', function (){return view('Capitulo_11/pagina89');});
+    Route::get('/5/11/90', function (){return view('Capitulo_11/pagina90');});
+    Route::get('/5/11/91', function (){return view('Capitulo_11/pagina91');});
+    Route::get('/5/11/92', function (){return view('Capitulo_11/pagina92');});
 
-    //Capitulo 12
+  //Capitulo 12
 
-            Route::get('/5/12/93', function (){return view('Capitulo_12/pagina93');});
-            Route::get('/5/12/94', function (){return view('Capitulo_12/pagina94');});
-            Route::get('/5/12/95', function (){return view('Capitulo_12/pagina95');});
-            Route::get('/5/12/96', function (){return view('Capitulo_12/pagina96');});
-            Route::get('/5/12/97', function (){return view('Capitulo_12/pagina97');});
-            Route::get('/5/12/98', function (){return view('Capitulo_12/pagina98');});
-            Route::get('/5/12/99', function (){return view('Capitulo_12/pagina99');});
-            Route::get('/5/12/100', function (){return view('Capitulo_12/pagina100');});
+    Route::get('/5/12/93', function (){return view('Capitulo_12/pagina93');});
+    Route::get('/5/12/94', function (){return view('Capitulo_12/pagina94');});
+    Route::get('/5/12/95', function (){return view('Capitulo_12/pagina95');});
+    Route::get('/5/12/96', function (){return view('Capitulo_12/pagina96');});
+    Route::get('/5/12/97', function (){return view('Capitulo_12/pagina97');});
+    Route::get('/5/12/98', function (){return view('Capitulo_12/pagina98');});
+    Route::get('/5/12/99', function (){return view('Capitulo_12/pagina99');});
+    Route::get('/5/12/100', function (){return view('Capitulo_12/pagina100');});
 
-   //Capitulo 13
+  //Capitulo 13
 
-            Route::get('/5/13/101', function (){return view('Capitulo_13/pagina101');});
-            Route::get('/5/13/102', function (){return view('Capitulo_13/pagina102');});
-            Route::get('/5/13/103', function (){return view('Capitulo_13/pagina103');});
-            Route::get('/5/13/104', function (){return view('Capitulo_13/pagina104');});
-            Route::get('/5/13/105', function (){return view('Capitulo_13/pagina105');});
-            Route::get('/5/13/106', function (){return view('Capitulo_13/pagina106');});
-            Route::get('/5/13/107', function (){return view('Capitulo_13/pagina107');});
+    Route::get('/5/13/101', function (){return view('Capitulo_13/pagina101');});
+    Route::get('/5/13/102', function (){return view('Capitulo_13/pagina102');});
+    Route::get('/5/13/103', function (){return view('Capitulo_13/pagina103');});
+    Route::get('/5/13/104', function (){return view('Capitulo_13/pagina104');});
+    Route::get('/5/13/105', function (){return view('Capitulo_13/pagina105');});
+    Route::get('/5/13/106', function (){return view('Capitulo_13/pagina106');});
+    Route::get('/5/13/107', function (){return view('Capitulo_13/pagina107');});
 
   //Capitulo 14
 
-            Route::get('/5/14/108', function (){return view('Capitulo_14/pagina108');});
-            Route::get('/5/14/109', function (){return view('Capitulo_14/pagina109');});
-            Route::get('/5/14/110', function (){return view('Capitulo_14/pagina110');});
-            Route::get('/5/14/111', function (){return view('Capitulo_14/pagina111');});
-            Route::get('/5/14/112', function (){return view('Capitulo_14/pagina112');});
-            Route::get('/5/14/113', function (){return view('Capitulo_14/pagina113');});
-            Route::get('/5/14/114', function (){return view('Capitulo_14/pagina114');});
+    Route::get('/5/14/108', function (){return view('Capitulo_14/pagina108');});
+    Route::get('/5/14/109', function (){return view('Capitulo_14/pagina109');});
+    Route::get('/5/14/110', function (){return view('Capitulo_14/pagina110');});
+    Route::get('/5/14/111', function (){return view('Capitulo_14/pagina111');});
+    Route::get('/5/14/112', function (){return view('Capitulo_14/pagina112');});
+    Route::get('/5/14/113', function (){return view('Capitulo_14/pagina113');});
+    Route::get('/5/14/114', function (){return view('Capitulo_14/pagina114');});
 
   //Capitulo 15
 
-            Route::get('/5/15/115', function (){return view('Capitulo_15/pagina115');});
-            Route::get('/5/15/116', function (){return view('Capitulo_15/pagina116');});
-            Route::get('/5/15/117', function (){return view('Capitulo_15/pagina117');});
-            Route::get('/5/15/118', function (){return view('Capitulo_15/pagina118');});
-            Route::get('/5/15/119', function (){return view('Capitulo_15/pagina119');});
-            Route::get('/5/15/120', function (){return view('Capitulo_15/pagina120');});
-            Route::get('/5/15/121', function (){return view('Capitulo_15/pagina121');});
-            Route::get('/5/15/122', function (){return view('Capitulo_15/pagina122');});
+    Route::get('/5/15/115', function (){return view('Capitulo_15/pagina115');});
+    Route::get('/5/15/116', function (){return view('Capitulo_15/pagina116');});
+    Route::get('/5/15/117', function (){return view('Capitulo_15/pagina117');});
+    Route::get('/5/15/118', function (){return view('Capitulo_15/pagina118');});
+    Route::get('/5/15/119', function (){return view('Capitulo_15/pagina119');});
+    Route::get('/5/15/120', function (){return view('Capitulo_15/pagina120');});
+    Route::get('/5/15/121', function (){return view('Capitulo_15/pagina121');});
+    Route::get('/5/15/122', function (){return view('Capitulo_15/pagina122');});
 
 // Capitulos 8 al 15 *******************************************************************************************************
+// Capitulos 16
+
+    Route::get('/1/16/123', function (){return view('Capitulo_16/Pagina_123');});
+    Route::get('/1/16/124', function (){return view('Capitulo_16/Pagina_124');});
+    Route::get('/1/16/125', function (){return view('Capitulo_16/Pagina_125');});
+    Route::get('/1/16/126', function (){return view('Capitulo_16/Pagina_126');});
+    Route::get('/1/16/127', function (){return view('Capitulo_16/Pagina_127');});
+    Route::get('/1/16/128', function (){return view('Capitulo_16/Pagina_128');});
+    Route::get('/1/16/129', function (){return view('Capitulo_16/Pagina_129');});
+    Route::get('/1/16/130', function (){return view('Capitulo_16/Pagina_130');});
+
+// Capitulos 17
+    Route::get('/1/17/131', function (){return view('Capitulo_17/Pagina_131');});
+    Route::get('/1/17/132', function (){return view('Capitulo_17/Pagina_132');});
+    Route::get('/1/17/133', function (){return view('Capitulo_17/Pagina_133');});
+    Route::get('/1/17/134', function (){return view('Capitulo_17/Pagina_134');});
+    Route::get('/1/17/135', function (){return view('Capitulo_17/Pagina_135');});
+    Route::get('/1/17/136', function (){return view('Capitulo_17/Pagina_136');});
+    Route::get('/1/17/137', function (){return view('Capitulo_17/Pagina_137');});
+    Route::get('/1/17/138', function (){return view('Capitulo_17/Pagina_138');});
+    Route::get('/1/17/139', function (){return view('Capitulo_17/Pagina_139');});
+
+// Capitulos 18
+    Route::get('/1/18/140', function (){return view('Capitulo_18/Pagina_140');});
+    Route::get('/1/18/141', function (){return view('Capitulo_18/Pagina_141');});
+    Route::get('/1/18/142', function (){return view('Capitulo_18/Pagina_142');});
+    Route::get('/1/18/143', function (){return view('Capitulo_18/Pagina_143');});
+    Route::get('/1/18/144', function (){return view('Capitulo_18/Pagina_144');});
+    Route::get('/1/18/145', function (){return view('Capitulo_18/Pagina_145');});
+    Route::get('/1/18/146', function (){return view('Capitulo_18/Pagina_146');});
+    Route::get('/1/18/147', function (){return view('Capitulo_18/Pagina_147');});
+
+// Capitulos 19
+    Route::get('/1/19/148', function (){return view('Capitulo_19/Pagina_148');});
+    Route::get('/1/19/149', function (){return view('Capitulo_19/Pagina_149');});
+    Route::get('/1/19/150', function (){return view('Capitulo_19/Pagina_150');});
+    Route::get('/1/19/151', function (){return view('Capitulo_19/Pagina_151');});
+    Route::get('/1/19/152', function (){return view('Capitulo_19/Pagina_152');});
+    Route::get('/1/19/153', function (){return view('Capitulo_19/Pagina_153');});
+    Route::get('/1/19/154', function (){return view('Capitulo_19/Pagina_154');});
+
+// Capitulos 20
+    Route::get('/1/20/155', function (){return view('Capitulo_20/Pagina_155');});
+    Route::get('/1/20/156', function (){return view('Capitulo_20/Pagina_156');});
+    Route::get('/1/20/157', function (){return view('Capitulo_20/Pagina_157');});
+    Route::get('/1/20/158', function (){return view('Capitulo_20/Pagina_158');});
+    Route::get('/1/20/159', function (){return view('Capitulo_20/Pagina_159');});
+    Route::get('/1/20/160', function (){return view('Capitulo_20/Pagina_160');});
+    Route::get('/1/20/161', function (){return view('Capitulo_20/Pagina_161');});
+
+// Capitulos 21
+    Route::get('/1/21/162', function (){return view('Capitulo_21/Pagina_162');});
+    Route::get('/1/21/163', function (){return view('Capitulo_21/Pagina_163');});
+    Route::get('/1/21/164', function (){return view('Capitulo_21/Pagina_164');});
+    Route::get('/1/21/165', function (){return view('Capitulo_21/Pagina_165');});
+    Route::get('/1/21/166', function (){return view('Capitulo_21/Pagina_166');});
+    Route::get('/1/21/167', function (){return view('Capitulo_21/Pagina_167');});
+    Route::get('/1/21/168', function (){return view('Capitulo_21/Pagina_168');});
+    Route::get('/1/21/169', function (){return view('Capitulo_21/Pagina_169');});
 ?>
